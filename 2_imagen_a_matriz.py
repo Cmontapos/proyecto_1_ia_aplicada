ea6e9da09d0f549989d37d3f0d449c85f8ccd8ee
Cristopher Montero Campos <cris04montero@gmail.com>
2026-05-12 09:21:13 -0600

    Cambios finales

--- 2_imagen_a_matriz.py
+++ 2_imagen_a_matriz.py
@@ -11,7 +11,7 @@ import os
 from pathlib import Path
 import json
 
-def imagen_a_matriz(ruta_imagen, umbral=145):
+def imagen_a_matriz(ruta_imagen, umbral=130):
     """
     Convierte una imagen en escala de grises a una matriz binaria
     
